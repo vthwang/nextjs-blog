72f4d42493cc4c8bad0b9fca1971ea4cf732e744
TingSyuanWang <vincent@fishboneapps.com>
2022-11-20 14:16:35 -0500

    feat: assets, metadata, and CSS
    Signed-off-by: TingSyuanWang <vincent@fishboneapps.com>

---
+++ pages/posts/first-post.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+        </Layout>
+    );
+}
